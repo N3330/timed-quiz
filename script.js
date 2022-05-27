97df4338a3f2a1a548c4514a48c0aa8c6dc29e57
James G <stackz@protonmail.com>
2022-05-27 00:20:17 -0400

    submitting what i have

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var startQuiz = document.querySelector('#start-btn');
+var timerEl = document.querySelector('#countdown');
 var index = 0
+var timeLeft = 60;
 startQuiz.addEventListener('click', function () {
     document.querySelector('#start').setAttribute("class", "hide");
     document.querySelector('#quiz').removeAttribute("class");
     buildQuiz();
+    
 });
 var questions = [
     {
@@ -32,29 +35,72 @@ var questions = [
         answer: "A"
     }
 ];
+
+
+function countdown() {
+  
+    var timer = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = timeLeft + ' seconds remaining.';
+  
+      if(timeLeft === 0) {
+        clearInterval(timer);
+        endGame();
+      }
+      
+    },1000);
+  }
+
+function endGame() {
+    showQuestions.textContent = "Finished!";
+    timeLeft = 0;
+    answer.textContent = ""
+
+
+}
+
+
 function buildQuiz() {
+    if (index === questions.length) {
+        console.log('endGame');
+
+        return;
+
+    }
+    countdown();
     var showQuestions = document.querySelector('#title');
     showQuestions.textContent = questions[index].text;
     var answerBox = document.querySelector('#answers');
     console.log(answerBox);
-    answerBox.innerHTML = ""; 
-    questions[index].choices.forEach(function(choice) {
-     console.log(choice);
-     var button = document.createElement('button');
-     button.textContent = choice;
-     button.setAttribute('value', choice)
-     button.onclick = function () {
-         index++
-         if (index === questions.length) {
-             console.log('endGame');
-         }
-        buildQuiz();
-     }
-     answerBox.appendChild(button);    
+    answerBox.innerHTML = "";
+    questions[index].choices.forEach(function (choice) {
+        var button = document.createElement('button');
+        button.textContent = choice;
+        button.setAttribute('value', choice)
+        button.onclick = function () {
+            var selectedAnswer = this.value;
+            console.log(selectedAnswer);
+            if (selectedAnswer == questions[index].answer) {
+                answerBox.textContent = "Correct answer!";
+                index++
+                buildQuiz();
+            } else  { answerBox.textContent = "Wrong answer!";
+            timeLeft -= 10; 
+            console.log(timeLeft);
+            index++
+            buildQuiz();
+        }
+        }
+        answerBox.appendChild(button);
     })
     //load the questions
     //append answer buttons 
     //add a click to the answer buttons 
     //when clicked find out if right or wrong 
     //reload function 
+    // create timer
+    // time left turned into score 
+    // high scores to local storage 
+    //timer displayed on the page 
+    //subtract time for wrong answers
 }
